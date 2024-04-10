25c473a10587f8900d2e180f8770c0390dd1c60f
Silla K <79395251+silla2807@users.noreply.github.com>
2024-04-10 17:38:08 +0530

    Create LinkedListCycle.java

---
+++ LinkedListCycle.java
@@ -0,0 +1,24 @@
+
+
+class Solution {
+
+     public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false; 
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast= fast.next.next;
+                if(slow == fast) {
+                    return true;
+                }
+                
+            }
+          
+        return false; 
+  }
+}
